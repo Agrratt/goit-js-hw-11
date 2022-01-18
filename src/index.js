28ea83b81510157cd0013a1a8f4fdff59f5c8f4a
Kateryna Tolstykh <agrratt@gmail.com>
2022-01-18 22:19:20 +0200

    add files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './css/common.css';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import { getRefs } from './js/getRefs';
+import { markupGallery } from './js/markupGallery';
+
+
+
+const refs = getRefs();
---
+++ src/js/markupGallery.js
@@ -0,0 +1,21 @@
+export function markupGallery() {
+    return `<div class="gallery">
+    <div class="photo-card">
+        <img src="" alt="" loading="lazy" />
+        <div class="info">
+            <p class="info-item">
+                <b>Likes</b>
+            </p>
+            <p class="info-item">
+                <b>Views</b>
+            </p>
+            <p class="info-item">
+                <b>Comments</b>
+            </p>
+            <p class="info-item">
+                <b>Downloads</b>
+            </p>
+        </div>
+    </div>
+</div>`
+}
